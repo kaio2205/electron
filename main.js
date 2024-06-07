fb20be4545cbcb3f05b9a52277f7c04bc2951b87
Kaio Eduardo <kaiocarvalhode@gmail.com>
2024-06-07 15:37:26 -0300

    atualizaçao

--- main.js
+++ main.js
@@ -1,16 +1,97 @@
-// console.log("Processo Principal")
+ console.log("Processo Principal")
 
-const { app, BrowserWindow } = require('electron')
+ const { app, BrowserWindow, nativeTheme,Menu,shell } = require('electron')  // Importaçao
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+ const createWindow = () => {       // Janela Principal 
+    // nativeTheme.themeSource ='dark'
+   const win = new BrowserWindow({       
+     width: 800, // largura 
+     height: 600,// altura
+     icon: './src/public/img/piggy.png'
+    //  resizable: false, // evitar o redimensionamento 
+    //  titleBarStyle:'hidden'  esconder barra de titulo e menu 
+    //  autoHideMenuBar: true // esconder menu 
+   })
+   Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
-//   win.loadFile('index.html')
+   win.loadFile('./src/views/index.html')
+ }
+ // janela sobre 
+ const aboutWindow = () => {       // Janela Principal 
+    // nativeTheme.themeSource ='dark'
+   const about = new BrowserWindow({       
+     width: 360, // largura 
+     height: 220,// altura
+     icon: './src/public/img/piggy.png',
+      resizable: false, // evitar o redimensionamento 
+     titleBarStyle:'hidden',  //esconder barra de titulo e menu 
+    // autoHideMenuBar: true // esconder menu 
+   })
+   
+   //iniciar a janela com o menu personalizado 
+  
+
+   about.loadFile('./src/views/sobre.html') 
 }
 
-app.whenReady().then(() => {
-  createWindow()
-})
+ app.whenReady().then(() => {  // executa de forma assincrona a aplicaçao 
+   createWindow()
+ })
+
+//  Template do menu personalizado
+
+const  template = [
+    {
+        label: 'Arquivo',
+        submenu:[
+            {
+                label: 'sair',
+                click:()=> app.quit(),
+                accelerator: 'Alt+F4'
+            }
+        ]
+    },
+    {
+        label: 'Exibir',
+        submenu:[
+            {
+              label:'Recarregar',
+              role: 'reload'  
+            },{
+                label:'Ferramentas do Desenvolvedor',
+                role:'toggleDevTools'
+            },{
+                type:'separator'
+
+            },{
+                label:'aplicar zoom',
+                role:'zoomIn'
+            },{
+                label:'Reduzir',
+                role:'zoomOut'
+            },{
+                label:'Restaurar o zoom padrao',
+                role:'resetZoom'
+            }
+        ]
+    },
+    {
+        label: 'Ajuda',
+        submenu:[
+            {
+                label:'docs',
+                click: ()=> shell.openExternal('https://www.electronjs.org/docs/latest/')
+            },
+            {
+                type:   'separator'
+            },
+            
+            {
+                label:  'Sobre',
+                accelerator:'Alt+F1',
+                click: ()=> aboutWindow()
+            },
+        ]
+    }
+
+]
