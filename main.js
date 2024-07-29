a901d2a784021400fb951abfb93ddbcbd482c979
Kaio Eduardo <kaiocarvalhode@gmail.com>
2024-07-29 13:57:56 -0300

    oi

--- main.js
+++ main.js
@@ -21,22 +21,72 @@ const createWindow = () => {       // Janela Principal
     win.loadFile('./src/views/index.html')
 }
 // janela sobre 
-const aboutWindow = () => {       // Janela Principal 
-    // nativeTheme.themeSource ='dark'
-    const about = new BrowserWindow({
-        width: 360, // largura 
-        height: 220,// altura
-        icon: './src/public/img/piggy.png',
-        resizable: false, // evitar o redimensionamento 
-        titleBarStyle: 'hidden',  //esconder barra de titulo e menu 
-        // autoHideMenuBar: true // esconder menu 
-    })
+let about // bug de abertura de vairas janelas
 
+
+
+const aboutWindow = () => {     // Janela sobre
+    
+    // se a janela about nao estiver aberta
+    if (!about) {  // bug (1)
+        about = new BrowserWindow({
+            width: 360, // largura 
+            height: 220,// altura
+            icon: './src/public/img/piggy.png',
+            resizable: false,  // evitar o redimensionamento 
+           // titleBarStyle: 'hidden',  esconder barra de titulo e menu 
+            // autoHideMenuBar: true // esconder menu 
+        })
+    }
     //iniciar a janela com o menu personalizado 
-
-
     about.loadFile('./src/views/sobre.html')
+     
+    // bug (2)
+    about.on('closed,',()=>{
+        about = null
+    })
 }
+
+
+// janela secundaria 
+
+
+const childwindow = ()=>{
+//  obten a jenela pai
+    const father = BrowserWindow.getFocusedWindow()
+
+    if (father) {
+        const child = new BrowserWindow({
+            width: 640,
+            height: 450,
+            icon: "./src/public/img/piggy.png",
+            autoHideMenuBar:true,
+            resizable: false,
+            parent: father,       // obtem a relaçao parent
+            modal: true // foco na janela 
+        })
+        child.loadFile("./src/views/child.html")
+    }
+
+    
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.whenReady().then(() => {  // executa de forma assincrona a aplicaçao 
     createWindow()
@@ -48,6 +98,13 @@ const template = [
     {
         label: 'Arquivo',
         submenu: [
+           { 
+            label:"janela secundaria",
+            click:()  => childwindow()
+
+           },
+           
+           
             {
                 label: 'sair',
                 click: () => app.quit(),
@@ -109,14 +166,14 @@ console.log(`Electron:${process.versions.electron}`)
 
 // Exemplo 2: recebimento de uma mensagem do renderer 
 
-ipcMain.on('send-message',(event,message) => {
+ipcMain.on('send-message', (event, message) => {
 
-console.log(`processo principal recebeu uma mensagem:${message}`)
+    console.log(`processo principal recebeu uma mensagem:${message}`)
 })
 
 
 // Exemplo 3 recebimento de uma açao a ser executada 
 
-ipcMain.on("open-about",()=>{
-aboutWindow()
+ipcMain.on("open-about", () => {
+    aboutWindow()
 })
